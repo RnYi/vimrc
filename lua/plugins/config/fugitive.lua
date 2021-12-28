56975a874b0547d29880e529cfe11e94828802f8
rany <rany97@qq.com>
2021-12-29 02:48:06 +0800

    增加fugitive快捷键。

--- lua/plugins/config/fugitive.lua
+++ lua/plugins/config/fugitive.lua
@@ -1,6 +1,9 @@
 local M = {}
 
 M.keybind = function ()
+  local map = vim.api.nvim_set_keymap
+  local map_opt = {noremap=true, silent=true}
+  map('n', '<Leader>gs', '<Cmd>Git<CR>', map_opt)
 end
 
 M.setup = function ()
