3a598b64bfea0e819c70205bb36950ad3b4cfecf
rany <rany97@qq.com>
2022-01-10 13:39:19 +0800

    update(colorscheme): enable bold style

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -49,6 +49,7 @@ require('packer').startup({
       config = function ()
         vim.g.gruvbox_material_better_performance=1
         vim.g.gruvbox_material_sign_column_background='none'
+        vim.g.gruvbox_material_enable_bold = 1
         vim.cmd('colorscheme gruvbox-material')
       end
     }
