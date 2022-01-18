66b0b3efa2d119d3acc10d180a29bc5a4d086ad8
rany <rany97@qq.com>
2022-01-18 10:56:39 +0800

    update(asynctasks): update configuration

--- lua/plugins/config/asynctasks.lua
+++ lua/plugins/config/asynctasks.lua
@@ -1,17 +1,19 @@
 local M = {}
 
 M.setup = function()
-    vim.cmd([[
-    augroup AsyncTasks
-        autocmd!
-        autocmd BufRead,BufNewFile .tasks set filetype=tasks
-        autocmd BufRead,BufNewFile tasks.ini set filetype=tasks
-    augroup END
-    ]])
+    -- vim.cmd([[
+    -- augroup AsyncTasks
+    --     autocmd!
+    --     autocmd BufRead,BufNewFile .tasks set filetype=tasks
+    --     autocmd BufRead,BufNewFile tasks.ini set filetype=tasks
+    -- augroup END
+    -- ]])
     vim.g.asyncrun_open=6
-    vim.g.asynctasks_term_pos='right'
+    vim.g.asynctasks_term_pos='TAB'
     vim.g.asynctasks_term_reuse=1
-    vim.g.asynctasks_extra_config={NvimHome..'/tasks.ini'}
+    vim.g.asynctasks_config_name='.asynctasks'
+    vim.g.asynctasks_rtp_config='asynctasks.ini'
+    vim.g.asynctasks_extra_config={NvimHome..'/asynctasks.ini'}
     vim.g.asyncrun_rootmarks={'.root','.project','.git','.hg',
                               '.svn','.projections.json'}
     vim.g.asynctasks_template={}
@@ -28,8 +30,6 @@ M.keybind = function ()
     map('i', '<F5>', '<Esc><Cmd>AsyncTask debug<CR>', map_opt)
     map('n', '<F6>', '<Cmd>AsyncTask git-add-commit<CR>', map_opt)
     map('i', '<F6>', '<Esc><Cmd>AsyncTask git-add-commit<CR>', map_opt)
-    map('n', '<F7>', '<Cmd>AsyncTask git-add-commit-push<CR>', map_opt)
-    map('i', '<F7>', '<Esc><Cmd>AsyncTask git-add-commit-push<CR>', map_opt)
 end
 
 return M
