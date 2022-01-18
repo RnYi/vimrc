4b90add75b373668d63c271095aa20bbb3ee1052
rany <rany97@qq.com>
2022-01-18 11:05:09 +0800

    update(nvim-cmp): enable trailing_slash for cmdline

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -62,7 +62,12 @@ M.setup = function()
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline(':', {
     sources = cmp.config.sources({
-      { name = 'path' }
+      {
+        name = 'path',
+        option = {
+          trailing_slash = true
+        }
+      }
     }, {
       { name = 'cmdline', keyword_pattern=[=[[^[:blank:]\!]*]=]}
     })
