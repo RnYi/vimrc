fe1817612d2f28c6edea2756b06b223d76ea7e71
rany <rany97@qq.com>
2022-03-23 14:45:02 +0800

    update(dirbuf): <M-d> to trash a file

--- lua/nvim/plugins/config/dirbuf.lua
+++ lua/nvim/plugins/config/dirbuf.lua
@@ -1,5 +1,11 @@
 local M = {}
 
+M.trash_file = function ()
+  local path = require('dirbuf').get_cursor_path()
+  vim.fn.execute('!trash '..path)
+  vim.fn.execute('e!')
+end
+
 M.keybind = function ()
   vim.keymap.set('n', '<Leader>e', '<Cmd>tabnew|Dirbuf<CR>')
 end
@@ -18,6 +24,7 @@ M.setup = function ()
     autocmd FileType dirbuf nnoremap <buffer> <C-t> <Cmd>lua require('dirbuf').enter('tabedit')<CR>
     autocmd FileType dirbuf nnoremap <buffer> <C-x> <Cmd>lua require('dirbuf').enter('split')<CR>
     autocmd FileType dirbuf nnoremap <buffer> <C-v> <Cmd>lua require('dirbuf').enter('vsplit')<CR>
+    autocmd FileType dirbuf nnoremap <buffer> <M-d> <Cmd>lua require('nvim.plugins.config.dirbuf').trash_file()<CR>
   augroup END
   ]]
 end
