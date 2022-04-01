c9564e72aa769c380ef779737d71d755d63b6b66
rany <rany97@qq.com>
2022-04-01 13:36:28 +0800

    update(plugins): open dirvish in cur win

--- lua/nvim/plugins/config/dirvish.lua
+++ lua/nvim/plugins/config/dirvish.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 M.keybind = function ()
-  vim.keymap.set('n', '<Leader>e', '<Cmd>tabnew|Dirvish<CR>')
+  vim.keymap.set('n', '<Leader>e', '<Cmd>Dirvish<CR>')
 end
 
 M.setup = function ()
-  
+  vim.cmd[[
+  let g:dirvish_mode=':sort ,^.*[\/],'
+  ]]
 end
 
 return M
